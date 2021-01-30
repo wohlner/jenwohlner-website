3b22775687e7cdda7e4fe5460971bdbabfd2743f
Jen Wohlner <jenwohlner@Jens-MBP.attlocal.net>
2021-01-30 14:45:25 -0600

    Update 404 page copy and layout

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import Container from '../components/container'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const NotFoundPage = () => (
   <Layout>
+    <Container>
     <SEO title='404: Not found' />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>You just hit a page on my site that doesn&#39;t exist... the sadness, the horror, the desolation.</p>
+    <p><a href="/">Go home!</a></p>
+    </Container>
   </Layout>
 )
 
